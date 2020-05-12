5b4fbdc376ffe04e98471c8c4965a4088b12521b
Luis Pabon <luis.pabon@auronconsulting.co.uk>
2020-05-12 16:02:16 +0100

    Revert "Don't transfer focus to NULL node on touch"
    This reverts commit dd736afbbd8c3970a31f8f408d3c0de603070e82.

--- sway/input/cursor.c
+++ sway/input/cursor.c
@@ -407,10 +407,6 @@ static void handle_touch_down(struct wl_listener *listener, void *data) {
 				event->touch_id, sx, sy);
 		cursor_hide(cursor);
 	}
-
-	if (focused_node) {
-		seat_set_focus(seat, focused_node);
-	}
 }
 
 static void handle_touch_up(struct wl_listener *listener, void *data) {
